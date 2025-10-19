be5075314f61db0835b84e7fa8000eae7dfa9e26
Kuenlun <kuenlunleal@gmail.com>
2025-10-19 06:26:52 +0200

    Test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print("This script is being run directly.")
 
+
 if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 import runpy
 
+
 def test_main():
     import main
+
     main.main()
     runpy.run_module("main", run_name="__main__")
